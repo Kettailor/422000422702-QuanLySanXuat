225f815f13d84c41e9b0cf0f3619b0abaadf8ca6
Kettailor <lekiet2409@gmail.com>
2025-12-25 00:16:58 +0700

    SQL

--- data/script.sql
+++ data/script.sql
@@ -703,10 +703,7 @@ INSERT INTO `vai_tro` (`IdVaiTro`, `TenVaiTro`) VALUES
 ('VT_KINH_DOANH', 'Nhân viên kinh doanh'),
 ('VT_NHANVIEN_KHO', 'Nhân viên kho'),
 ('VT_NHANVIEN_SANXUAT', 'Nhân viên sản xuất'),
-('VT_TRUONG_XUONG_KIEM_DINH', 'Xưởng trưởng kiểm định'),
-('VT_TRUONG_XUONG_LAP_RAP_DONG_GOI', 'Xưởng trưởng lắp ráp và đóng gói'),
-('VT_TRUONG_XUONG_LUU_TRU', 'Xưởng trưởng lưu trữ hàng hóa'),
-('VT_TRUONG_XUONG_SAN_XUAT', 'Xưởng trưởng sản xuất');
+('VT_NHANVIEN_KIEM_DINH', 'Nhân viên kiểm định chất lượng');
 
 -- --------------------------------------------------------
 
